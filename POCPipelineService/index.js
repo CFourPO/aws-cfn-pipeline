20e9e45e0564b34eff5facfc82da3ac9cffdbbf3
chris-nyllabs <chris@nyllabs.tech>
2017-02-18 14:31:17 -0500

    initial commit

---
+++ POCPipelineService/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chrispomerville on 2/18/17.
+ */
+var time = require('time');
+exports.handler = (event, context, callback) => {
+    var currentTime = new time.Date();
+    currentTime.setTimezone("America/Los_Angeles");
+    callback(null, {
+        statusCode: '200',
+        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+    });
+};
